d4ed7f640005f030a1d96f7f797f4a1dc790d856
zouhaierbarrouta <barrouta.zouhaier@gmail.com>
2026-03-24 00:33:45 +0100

    add light and darck mode

---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  private currentTheme: 'light' | 'dark' = 'dark';
+
+  constructor() {
+    const savedTheme = localStorage.getItem('theme') as 'light' | 'dark';
+    if (savedTheme) {
+      this.setTheme(savedTheme);
+    } else {
+      this.setTheme('dark'); // Default
+    }
+  }
+
+  toggleTheme() {
+    const newTheme = this.currentTheme === 'light' ? 'dark' : 'light';
+    this.setTheme(newTheme);
+  }
+
+  setTheme(theme: 'light' | 'dark') {
+    this.currentTheme = theme;
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+  }
+
+  getTheme() {
+    return this.currentTheme;
+  }
+}
